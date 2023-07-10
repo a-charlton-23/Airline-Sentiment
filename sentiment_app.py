d8bd26c93bc2c8dee614cb9868955259685d6844
a-charlton-23 <138829121+a-charlton-23@users.noreply.github.com>
2023-07-10 11:34:39 -0400

    Update sentiment_app.py

--- sentiment_app.py
+++ sentiment_app.py
@@ -45,15 +45,6 @@ if not st.sidebar.checkbox("Hide", True, key='0'):
         fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
 
-st.sidebar.subheader("When and where are users tweeting from?")
-hour = st.sidebar.slider("Hour to look at", 0, 23)
-modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='1'):
-    st.markdown("### Tweet locations based on time of day")
-    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
-    st.map(modified_data)
-    if st.sidebar.checkbox("Show raw data", False):
-        st.write(modified_data)
 
 
 st.sidebar.subheader("Total number of tweets for each airline")
