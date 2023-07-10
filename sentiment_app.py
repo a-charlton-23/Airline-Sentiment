47dd4df2164386fde98fd6163c9fdeab4bd07183
a-charlton-23 <138829121+a-charlton-23@users.noreply.github.com>
2023-07-10 11:30:40 -0400

    Update sentiment_app.py

--- sentiment_app.py
+++ sentiment_app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 DATA_URL = (
-    "/home/cicada/Downloads/rhyme/streamlit-sentiment/Tweets.csv"
+    "Tweets.csv"
 )
 
 st.title("Sentiment Analysis of Tweets about US Airlines")
@@ -48,7 +48,7 @@ if not st.sidebar.checkbox("Hide", True):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='1'):
+if not st.sidebar.checkbox("Close", True):
     st.markdown("### Tweet locations based on time of day")
     st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -57,10 +57,10 @@ if not st.sidebar.checkbox("Close", True, key='1'):
 
 
 st.sidebar.subheader("Total number of tweets for each airline")
-each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
+each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'])
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
+if not st.sidebar.checkbox("Close", True):
     if each_airline == 'Bar plot':
         st.subheader("Total number of tweets for each airline")
         fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -83,7 +83,7 @@ st.sidebar.subheader("Breakdown airline by sentiment")
 choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'))
 if len(choice) > 0:
     st.subheader("Breakdown airline by sentiment")
-    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key='3')
+    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ])
     fig_3 = make_subplots(rows=1, cols=len(choice), subplot_titles=choice)
     if breakdown_type == 'Bar plot':
         for i in range(1):
@@ -105,7 +105,7 @@ if len(choice) > 0:
         fig_3.update_layout(height=600, width=800)
         st.plotly_chart(fig_3)
 st.sidebar.subheader("Breakdown airline by sentiment")
-choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'), key=0)
+choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'))
 if len(choice) > 0:
     choice_data = data[data.airline.isin(choice)]
     fig_0 = px.histogram(
@@ -117,7 +117,7 @@ if len(choice) > 0:
 
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Close", True):
     st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
